248d7bf48bd1f74248b88a9062c7a49f8a3af595
RadoslawJanik <djatomikan@gmail.comgit config --global user.email djatomikan@gmail.comgit config --global user.email djatomikan@gmail.com>
2024-02-15 23:53:59 +0100

    Comments model

--- app/views.py
+++ app/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
+from app.forms import CommentForm
 
 from app.models import Post
 
 # Create your views here.
-
-
-def post_page(request,slug):
-    post = Post.objects.get(slug=slug)
-
-    context = {'post':post}
-    return render(request, 'app/post.html',context)
-
 def index(request):
     posts = Post.objects.all()
     context = {'posts':posts}
-    return render(request,'app/index.html',context)
+    return render(request, 'app/index.html', context)
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    form = CommentForm()
+
+    if request.POST:
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid:
+            comment = comment_form.save(commit=False)
+            postid = request.POST.get('post_id')
+            post = Post.objects.get(id = postid)
+            comment.post = post
+            comment.save()
+
+    if post.view_count is None:
+        post.view_count = 1
+    else:
+        post.view_count = post.view_count + 1
+    post.save()
+    
+    context = {'post':post, 'form':form}
+    return render(request, 'app/post.html', context)
     
