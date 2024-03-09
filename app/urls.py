db6d45b27e8df50d38d2c9f9d2749b24fabd01aa
RadoslawJanik <djatomikan@gmail.comgit config --global user.email djatomikan@gmail.comgit config --global user.email djatomikan@gmail.com>
2024-03-09 17:57:50 +0100

    liked posts page, bookmark page, side bar

--- app/urls.py
+++ app/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('author/<slug:slug>',views.author_page,name='author_page'),
     path('search/',views.search_posts,name='search'),
     path('about/',views.about,name='about'),
-    path('accounts/register/',views.register_user,name='register')
+    path('accounts/register/',views.register_user,name='register'),
+    path('bookmark_post/<slug:slug>',views.bookmark_post,name='bookmark_post'),
+    path('like_post/<slug:slug>',views.like_post,name='like_post'),
+    path('all_bookmarked_posts',views.all_bookmarked_posts,name='all_bookmarked_posts'),
+    path('all_posts',views.all_posts,name='all_posts'),
+    path('all_liked_posts',views.all_liked_posts,name='all_liked_posts')
 ]
